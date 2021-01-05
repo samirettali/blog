f37dddd3e5d633424bb7a9bafdd67f52f71073e7
Samir Ettali <ettali.samir@gmail.com>
2021-01-05 23:32:15 +0100

    Refactored styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,13 @@ module.exports = {
       sans: ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       monospace: [ "SFMono-Regular", "Menlo", "Consolas","Liberation Mono", "monospace" ],
     },
-    colors: {
-      ...colors,
-      gray: colors.warmGray,
-    },
     extend: {
       colors: {
         mygray: {
           light: "#e4e4e4",
           DEFAULT: "#222831",
           dark: "#2b2d3e",
+          secondaryDark: "#263238",
         },
         accent: {
           DEFAULT: "#ef4f4f",

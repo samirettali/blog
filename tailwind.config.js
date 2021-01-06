65611e9847877f5701caa0c57ce52c57b77bf6f3
Samir Ettali <ettali.samir@gmail.com>
2021-01-06 21:58:33 +0100

    Changed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,17 @@ module.exports = {
     fontFamily: {
       serif: ["Roboto Slab", "serif"],
       sans: ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      monospace: [ "SFMono-Regular", "Menlo", "Consolas","Liberation Mono", "monospace" ],
+      mono: [ "SFMono-Regular", "Menlo", "Consolas","Liberation Mono", "monospace" ],
     },
     extend: {
       colors: {
         mygray: {
           light: "#e4e4e4",
-          DEFAULT: "#222831",
+          // DEFAULT: "#222831",
+          DEFAULT: "#24292e",
           dark: "#2b2d3e",
-          secondaryDark: "#263238",
+          // secondaryDark: "#263238",
+          secondaryDark: "#1d2125",
         },
         accent: {
           DEFAULT: "#ef4f4f",

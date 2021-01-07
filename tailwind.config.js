2761710999acfe51912b1b81083c5a1c7112e247
Samir Ettali <ettali.samir@gmail.com>
2021-01-07 01:15:01 +0100

    Styles code blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     fontFamily: {
       serif: ["Roboto Slab", "serif"],
       sans: ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      mono: [ "SFMono-Regular", "Menlo", "Consolas","Liberation Mono", "monospace" ],
+      mono: [ "Menlo", "Consolas", "Liberation Mono", "monospace" ],
     },
     extend: {
       colors: {

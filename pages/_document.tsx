431262a37ba93d79aafdaf1254243a9e2cbeadf2
Samir Ettali <ettali.samir@gmail.com>
2021-02-11 01:11:11 +0100

    Removed old dark mode stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,55 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
-// function setInitialColorMode() {
-//   function getInitialColorMode() {
-//     const persistedColorPreference = window.localStorage.getItem("theme");
-//     const hasPersistedPreference = typeof persistedColorPreference === "string";
-
-//     if (hasPersistedPreference) {
-//       return persistedColorPreference;
-//     }
-
-//     const mql = window.matchMedia("(prefers-color-scheme: dark)");
-//     const hasMediaQueryPreference = typeof mql.matches === "boolean";
-
-//     if (hasMediaQueryPreference) {
-//       return mql.matches ? "dark" : "light";
-//     }
-
-//     return "light";
-//   }
-
-//   const colorMode = getInitialColorMode();
-//   const root = document.documentElement;
-//   root.style.setProperty("--initial-color-mode", colorMode);
-
-//   if (colorMode === "dark") {
-//     document.querySelector('html').classList.add('dark')
-//     // document.documentElement.setAttribute("data-theme", "dark");
-//   } else {
-//     document.querySelector('html').classList.remove('dark')
-//   }
-
-// }
-
-// const blockingSetInitialColorMode = `(function() {
-//   ${setInitialColorMode.toString()}
-//   setInitialColorMode();
-// })()
-// `;
-
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head />
         <body>
-          {/* <script
-            dangerouslySetInnerHTML={{
-              __html: blockingSetInitialColorMode,
-            }}
-          >
-          </script> */}
           <Main />
           <NextScript />
         </body>

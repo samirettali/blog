178cfd886d52d9a0492fb2c4de69934d251311ed
Samir Ettali <ettali.samir@gmail.com>
2021-02-11 11:59:14 +0100

    Returned to redirects

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 const withMDX = require("@next/mdx")();
 
-const rewrites = async () => {
+const redirects = async () => {
   return [
     {
       source: "/cv",
       destination: "https://samir-resume.vercel.app",
-      basePath: false,
     },
   ];
 };
 
 module.exports = {
   ...withMDX(),
-  rewrites,
+  redirects,
   reactStrictMode: true,
 };

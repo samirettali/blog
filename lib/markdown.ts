bcc54a76b2e51ae3a6fb1e51906a856bd5c82274
Samir Ettali <ettali.samir@gmail.com>
2021-02-21 17:00:07 +0100

    Added remark-external-links for target='_blank'

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,17 +1,19 @@
-import unified from 'unified';
-import markdown from 'remark-parse';
+import unified from "unified";
+import markdown from "remark-parse";
 import html from "remark-html";
 import prism from "remark-prism";
-import slug from 'remark-slug';
+import slug from "remark-slug";
+import externalLinks from "remark-external-links";
 
 const markdownToHtml = async (content: string) => {
-  const result = await unified()
+	const result = await unified()
+		.use(externalLinks, { target: "_blank", rel: ["noopener"] })
 		.use(markdown)
 		.use(slug)
 		.use(html)
-		.use(prism, { transformInlineCode: true})
-		.process(content)
+		.use(prism, { transformInlineCode: true })
+		.process(content);
 	return result.toString();
-}
+};
 
-export default markdownToHtml
+export default markdownToHtml;

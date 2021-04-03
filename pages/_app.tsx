673d50fb44b5c38dde261dd99b0a5fde6a023580
Samir Ettali <ettali.samir@gmail.com>
2021-04-03 20:27:56 +0200

    Fixed initial theme bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" enableSystem={false}>
       <Component {...pageProps} />
     </ThemeProvider>
   )

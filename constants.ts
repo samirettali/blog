2fd99b3f6d11faf14065fa8f4dc64c68aafe9f38
Samir Ettali <ettali.samir@gmail.com>
2021-04-09 22:15:46 +0200

    Added constant

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
 export const IS_PROD = process.env.NODE_ENV === 'production';
+export const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
 export const CONTENT_PATH = 'content';
 export const RSS_FEED_PATH = './public/rss.xml';

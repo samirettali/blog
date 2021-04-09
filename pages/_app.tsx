3b328ca1e5bcd093f08bb3b281b842137556389d
Samir Ettali <ettali.samir@gmail.com>
2021-04-09 22:11:23 +0200

    Added panelbear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
-import "../styles/main.css";
 import { ThemeProvider } from "next-themes";
+import { usePanelbear } from "./../hooks/panelbear";
+import { PANELBEAR_SITE_ID } from '../constants'
+
+import "../styles/main.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (PANELBEAR_SITE_ID) {
+    usePanelbear(PANELBEAR_SITE_ID, {});
+  }
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
       <Component {...pageProps} />

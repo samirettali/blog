b70681bd282a1948221392850453468f0ac36214
Samir Ettali <ettali.samir@gmail.com>
2021-04-10 00:04:43 +0200

    How on Earth that could've worked...

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export const IS_PROD = process.env.NODE_ENV === 'production';
-export const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
+export const PANELBEAR_SITE_ID = '7G460XxmP4F';
 export const CONTENT_PATH = 'content';
 export const RSS_FEED_PATH = './public/rss.xml';
